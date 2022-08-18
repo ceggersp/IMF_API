894ecf8218eea9c18cdc11e6987b22586dc2d87c
ceggersp <ceggers@fen.uchile.cl>
2022-08-18 17:17:13 -0400

    Linux setup

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 import sys
 import os
-wpath = sys.path[0]
-ppath = sys.path[5]+'\\PyIMF'
-os.system('mkdir '+ppath)
-os.system('del '+ppath+'\\functions.py')
-os.system('del '+ppath+'\\__init__.py')
-os.system('copy '+'"'+wpath+'\\functions.py" '+ppath)
-os.system('copy '+'"'+wpath+'\\__init__.py" '+ppath)
+import platform
+
+if platform.system() == 'Linux':
+
+    wpath = sys.path[0]
+    ppath = sys.path[5]+'/PyIMF'
+    os.system('mkdir '+ppath)
+    os.system('rm '+ppath+'/functions.py')
+    os.system('rm '+ppath+'/__init__.py')
+    os.system('cp '+'"'+wpath+'/functions.py" '+ppath)
+    os.system('cp '+'"'+wpath+'/__init__.py" '+ppath)
+
+else:
+
+    wpath = sys.path[0]
+    ppath = sys.path[5]+'\\PyIMF'
+    os.system('mkdir '+ppath)
+    os.system('del '+ppath+'\\functions.py')
+    os.system('del '+ppath+'\\__init__.py')
+    os.system('copy '+'"'+wpath+'\\functions.py" '+ppath)
+    os.system('copy '+'"'+wpath+'\\__init__.py" '+ppath)
