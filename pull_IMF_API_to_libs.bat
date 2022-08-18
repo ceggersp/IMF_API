3fb8783ff44071d766211e2c658e7791be69c34a
ceggersp <ceggers@fen.uchile.cl>
2022-08-18 18:18:47 -0400

    deleting stuff

--- pull_IMF_API_to_libs.bat
+++
@@ -1,4 +0,0 @@
-mkdir
-del "C:\Users\CARLOSEG\AppData\Local\Programs\Python\Python39\Lib\site-packages\functions.py"
-copy "C:\Users\CARLOSEG\OneDrive - Inter-American Development Bank Group\Documents\IMF_API\functions.py" "C:\Users\CARLOSEG\AppData\Local\Programs\Python\Python39\Lib\site-packages"
-pause
