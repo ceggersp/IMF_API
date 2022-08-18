a8ce51ed93d8ea03ccf6efb087576e824be39e19
ceggersp <ceggers@fen.uchile.cl>
2022-08-18 16:48:59 -0400

    rename

--- pull_IMF_API_to_libs.bat
+++ pull_IMF_API_to_libs.bat
@@ -1,3 +1,3 @@
-del "C:\Users\CARLOSEG\AppData\Local\Programs\Python\Python39\Lib\site-packages\IMF_API.py"
-copy "C:\Users\CARLOSEG\OneDrive - Inter-American Development Bank Group\Documents\IMF_API\IMF_API.py" "C:\Users\CARLOSEG\AppData\Local\Programs\Python\Python39\Lib\site-packages"
+del "C:\Users\CARLOSEG\AppData\Local\Programs\Python\Python39\Lib\site-packages\functions.py"
+copy "C:\Users\CARLOSEG\OneDrive - Inter-American Development Bank Group\Documents\IMF_API\functions.py" "C:\Users\CARLOSEG\AppData\Local\Programs\Python\Python39\Lib\site-packages"
 pause
