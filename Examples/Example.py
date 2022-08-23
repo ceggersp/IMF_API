be10a34af0ee4fedd9659935659b90bc0c922b73
ceggersp <ceggers@fen.uchile.cl>
2022-08-23 16:56:18 -0400

    Update

--- Examples/Example.py
+++ Examples/Example.py
@@ -1,11 +1,2 @@
 import IMF_API
-dir = 'C:/Users/CARLOSEG/OneDrive - Inter-American Development Bank Group/Documents/IMF_API/Data'
-
-dataset = 'IFS'
-indicator = 'PMP_IX'
-F = 'A'
-country = 'GB'
-
-DATA = IMF_API.request_data(dataset,country,indicator)
-
-print(DATA)
+print(IMF_API.request_data('FM','G_XWDG_G01_GDP_PT'))
